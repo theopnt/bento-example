98fb371782c21d4b58e8fb8cc3ccda70d427dfcc
theopnt <theopountridis@gmail.com>
2024-01-16 18:00:51 +0100

    Change bento to run for UC7

--- .ipynb_checkpoints/service-checkpoint.py
+++ .ipynb_checkpoints/service-checkpoint.py
@@ -1,7 +1,6 @@
 # service.py
 import pandas as pd
 import bentoml
-#import scikit-learn
 from bentoml import mlflow
 from bentoml.io import PandasDataFrame
 from bentoml.io import Text
@@ -9,39 +8,37 @@ from bentoml.io import Multipart
 from bentoml.exceptions import BentoMLException
      
 # Load the runner for the latest model we just saved
-lgbm_italy_runner = bentoml.mlflow.get("lgbm_italy:b2xpt4wsbw7m76qw").to_runner()
+model_runner = bentoml.mlflow.get("uc7_companies_no_covs:latest").to_runner()
 
-# Create the iris_classifier service with the ScikitLearn runner
-# Multiple runners may be specified if needed in the runners array
-# When packaged as a bento, the runners here will included
-lgbm_italy_service = bentoml.Service("lgbm_italy", runners=[lgbm_italy_runner])
+#creating the service for the model. We can name it whatever we wish
+model_service = bentoml.Service("uc7_companies_no_covs", runners=[model_runner])
 
 input_spec = Multipart(n=Text(), 
-                       series_uri=Text(),
-                       roll_size=Text(),
-                       future_covariates_uri=Text(),
-                       past_covariates_uri=Text(),
-                       batch_size=Text())
-
-@lgbm_italy_service.api(input=input_spec, output=Text())
+                       meter_id=Text(),
+                       series=PandasDataFrame())
+#input must be multipart, as it contains both strings and dataframes. Dataframes are converted from json
+#to dataframe by bentoml during the api call
+@model_service.api(input=input_spec, output=Text())
 def predict(n : str, 
-            series_uri : str,
-            roll_size : str,
-            future_covariates_uri : str,
-            past_covariates_uri : str,
-            batch_size : str) -> pd.DataFrame:
+            meter_id: str,
+            series : pd.DataFrame,
+            ) -> pd.DataFrame:
     try:
         n = int(n)        
     except:
-        raise BentoMLException("Can't convert parameters to int")
-            
+        raise BentoMLException("Can't convert parameters to int")            
     model_input = {
+        "multiple": "False",
+        "weather_covariates":[],
+        "resolution": "60",
+        "ts_id_pred": meter_id,        
         "n": n,
-        "series_uri": series_uri,
-        "roll_size": roll_size,
-        "future_covariates_uri": future_covariates_uri,
-        "past_covariates_uri": past_covariates_uri,
-        "batch_size": batch_size}
-    print(model_input)
-    res = lgbm_italy_runner.predict.run([model_input])
-    return str(res[0].to_dict())
+        "history": series,
+        "series_uri": "",
+        "roll_size": "1",
+        "future_covariates_uri": "None",
+        "past_covariates_uri": "None",
+        "batch_size": 1}
+    print(series)
+    res = model_runner.predict.run([model_input])
+    return res[0].to_json()
--- service.py
+++ service.py
@@ -1,7 +1,6 @@
 # service.py
 import pandas as pd
 import bentoml
-#import scikit-learn
 from bentoml import mlflow
 from bentoml.io import PandasDataFrame
 from bentoml.io import Text
@@ -9,18 +8,19 @@ from bentoml.io import Multipart
 from bentoml.exceptions import BentoMLException
      
 # Load the runner for the latest model we just saved
-lgbm_uc6_w6_pos_ac_runner = bentoml.mlflow.get("lgbm_uc6_w6_pos_ac:latest").to_runner()
+model_runner = bentoml.mlflow.get("uc7_companies_no_covs:latest").to_runner()
 
-# Create the iris_classifier service with the ScikitLearn runner
-# Multiple runners may be specified if needed in the runners array
-# When packaged as a bento, the runners here will included
-lgbm_uc6_w6_pos_ac_service = bentoml.Service("lgbm_uc6_w6_pos_ac", runners=[lgbm_uc6_w6_pos_ac_runner])
+#creating the service for the model. We can name it whatever we wish
+model_service = bentoml.Service("uc7_companies_no_covs", runners=[model_runner])
 
 input_spec = Multipart(n=Text(), 
+                       meter_id=Text(),
                        series=PandasDataFrame())
-
-@lgbm_uc6_w6_pos_ac_service.api(input=input_spec, output=Text())
+#input must be multipart, as it contains both strings and dataframes. Dataframes are converted from json
+#to dataframe by bentoml during the api call
+@model_service.api(input=input_spec, output=Text())
 def predict(n : str, 
+            meter_id: str,
             series : pd.DataFrame,
             ) -> pd.DataFrame:
     try:
@@ -28,13 +28,17 @@ def predict(n : str,
     except:
         raise BentoMLException("Can't convert parameters to int")            
     model_input = {
+        "multiple": "False",
+        "weather_covariates":[],
+        "resolution": "60",
+        "ts_id_pred": meter_id,        
         "n": n,
         "history": series,
         "series_uri": "",
-        "roll_size": "24",
+        "roll_size": "1",
         "future_covariates_uri": "None",
         "past_covariates_uri": "None",
         "batch_size": 1}
     print(series)
-    res = lgbm_uc6_w6_pos_ac_runner.predict.run([model_input])
+    res = model_runner.predict.run([model_input])
     return res[0].to_json()
